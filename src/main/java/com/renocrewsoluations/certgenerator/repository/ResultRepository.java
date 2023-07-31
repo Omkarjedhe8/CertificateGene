cd9f87d46c32507adbc7da71ff5479de69e4ccb8
Omkarjedhe8 <135060417+Omkarjedhe8@users.noreply.github.com>
2023-07-31 16:07:22 +0530

    Update ResultRepository.java

--- src/main/java/com/renocrewsoluations/certgenerator/repository/ResultRepository.java
+++ src/main/java/com/renocrewsoluations/certgenerator/repository/ResultRepository.java
@@ -1,7 +1,8 @@
 package com.renocrewsoluations.certgenerator.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.renocrewsoluations.certgenerator.entity.Result;
@@ -9,4 +10,7 @@ import com.renocrewsoluations.certgenerator.entity.Result;
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Long> {
 
+	public Result deleteByresultId(Long resultId);	
+	 public Optional<Result> findByresultId(Long resultId);
+	
 }
