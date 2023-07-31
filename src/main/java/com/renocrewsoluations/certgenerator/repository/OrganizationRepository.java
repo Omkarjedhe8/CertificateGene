3a104f34009fc60593fb0ad962405f80493f48db
Omkarjedhe8 <135060417+Omkarjedhe8@users.noreply.github.com>
2023-07-31 16:06:59 +0530

    Update OrganizationRepository.java

--- src/main/java/com/renocrewsoluations/certgenerator/repository/OrganizationRepository.java
+++ src/main/java/com/renocrewsoluations/certgenerator/repository/OrganizationRepository.java
@@ -1,12 +1,18 @@
 package com.renocrewsoluations.certgenerator.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
-import com.renocrewsoluations.certgenerator.entity.Organization;
+import com.renocrewsoluations.certgenerator.entity.Exam;
 
 @Repository
-public interface OrganizationRepository  extends JpaRepository<Organization, Long>{
+public interface ExamRepository extends JpaRepository<Exam, Long> {
 
+	 public Optional<Exam> findByexamId(Long examId);
+		
+	public Exam deleteByexamId(Long examId);	
+	 
 }
