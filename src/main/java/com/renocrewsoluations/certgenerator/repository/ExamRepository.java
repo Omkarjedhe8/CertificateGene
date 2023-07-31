a377d8a605360914e12149066a7346c60ea91841
Omkarjedhe8 <135060417+Omkarjedhe8@users.noreply.github.com>
2023-07-31 16:06:23 +0530

    Update ExamRepository.java

--- src/main/java/com/renocrewsoluations/certgenerator/repository/ExamRepository.java
+++ src/main/java/com/renocrewsoluations/certgenerator/repository/ExamRepository.java
@@ -1,5 +1,7 @@
 package com.renocrewsoluations.certgenerator.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,8 @@ import com.renocrewsoluations.certgenerator.entity.Exam;
 @Repository
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 
+	 public Optional<Exam> findByexamId(Long examId);
+		
+	public Exam deleteByexamId(Long examId);	
+	 
 }
