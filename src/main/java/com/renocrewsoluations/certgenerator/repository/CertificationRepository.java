15ea5d6102692b467e164ab4025d517c2eb34cd1
Omkarjedhe8 <135060417+Omkarjedhe8@users.noreply.github.com>
2023-07-31 16:05:49 +0530

    Update CertificationRepository.java

--- src/main/java/com/renocrewsoluations/certgenerator/repository/CertificationRepository.java
+++ src/main/java/com/renocrewsoluations/certgenerator/repository/CertificationRepository.java
@@ -1,12 +1,17 @@
 package com.renocrewsoluations.certgenerator.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.renocrewsoluations.certgenerator.entity.Certification;
 
 @Repository
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
+	
+	 public Optional<Certification> findById(Long certiId);
+	  public void deleteById(Long certiId);
+
 
 }
