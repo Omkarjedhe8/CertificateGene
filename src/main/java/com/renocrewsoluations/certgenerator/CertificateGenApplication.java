d962fb5211bfd2aedd54a6aee71f38e9b527910d
Omkarjedhe8 <135060417+Omkarjedhe8@users.noreply.github.com>
2023-07-31 16:15:44 +0530

    Update CertificateGenApplication.java

--- src/main/java/com/renocrewsoluations/certgenerator/CertificateGenApplication.java
+++ src/main/java/com/renocrewsoluations/certgenerator/CertificateGenApplication.java
@@ -2,10 +2,14 @@ package com.renocrewsoluations.certgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.renocrewsoluations.cretigenerator.CertificateGenApplication" }) 
+@EntityScan(" com.renocrewsoluations.certgenerator.repository")
+@EnableJpaRepositories("com.renocrewsoluations.certgenerator.repository.CertificationRepository")
 public class CertificateGenApplication {
 
 	public static void main(String[] args) {
