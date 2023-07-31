b47cfc4eec4f793cc44f17d0b9f850cec5bc53d3
Omkarjedhe8 <135060417+Omkarjedhe8@users.noreply.github.com>
2023-07-31 16:10:29 +0530

    Update UserRepository.java

--- src/main/java/com/renocrewsoluations/certgenerator/repository/UserRepository.java
+++ src/main/java/com/renocrewsoluations/certgenerator/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package com.renocrewsoluations.certgenerator.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.renocrewsoluations.certgenerator.entity.User;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	 public Optional<User> findById(Long id);
+	  public void deleteById(Long id);
+			   
 }
